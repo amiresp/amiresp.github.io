6991d71819d69f6863803aabdf18f0ff16d8739f
Amir Spalani <abasirat92@gmail.com>
2022-08-03 19:30:07 +0430

    chore(chore): battery status and time

---
+++ assets/js/main.js
@@ -0,0 +1,46 @@
+'use strict';
+
+let batteryIsCharging = false;
+
+navigator.getBattery().then(function (battery) {
+  batteryIsCharging = battery.charging;
+
+  function updateLevelInfo() {
+    console.log(`Battery level: ${battery.level * 100}%`);
+    document.querySelectorAll('.battery rect')[1].style.width = `${
+      battery.level * 100
+    }%`;
+  }
+  function chargingChangeInfo(batteryIsCharging) {
+    document
+      .querySelectorAll('.notification-bar')[0]
+      .classList.remove(`is-chargin-true`);
+    document
+      .querySelectorAll('.notification-bar')[0]
+      .classList.remove(`is-chargin-false`);
+    document
+      .querySelectorAll('.notification-bar')[0]
+      .classList.add(`is-chargin-${batteryIsCharging}`);
+  }
+  battery.addEventListener('levelchange', () => {
+    console.log(`Battery level: ${battery.level * 100}%`);
+    updateLevelInfo();
+  });
+
+  battery.addEventListener('chargingchange', function () {
+    batteryIsCharging = battery.charging;
+    chargingChangeInfo(batteryIsCharging);
+  });
+  updateLevelInfo();
+  chargingChangeInfo(batteryIsCharging);
+});
+
+function prettyDate() {
+  var date = new Date();
+  return date.toLocaleTimeString(navigator.language, {
+    hour: '2-digit',
+    minute: '2-digit',
+  });
+}
+
+document.querySelectorAll('.time')[0].textContent = prettyDate();
